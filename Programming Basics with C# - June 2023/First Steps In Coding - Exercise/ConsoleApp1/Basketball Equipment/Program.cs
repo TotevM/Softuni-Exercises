09f2ccfbc23f97b669deeb39e61c1c8b76116c69
TotevM <150013904+TotevM@users.noreply.github.com>
2024-01-14 10:46:29 +0200

    Delete Programming Basics with C# - June 2023/First Steps In Coding - Exercise/ConsoleApp1 directory

--- Programming Basics with C# - June 2023/First Steps In Coding - Exercise/ConsoleApp1/Basketball Equipment/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿int tax = int.Parse(Console.ReadLine());
-
-double boots = tax * 0.6;
-double suit = boots * 0.8;
-double ball = suit * 0.25;
-double accessories = ball * 0.2;
-
-double fulltax = tax + boots + suit + ball + accessories;
-Console.WriteLine(fulltax);
--- Programming Basics with C# - June 2023/First Steps In Coding - Exercise/ConsoleApp1/USD to BGN/Program.cs
+++
@@ -1,4 +0,0 @@
-﻿double usd = double.Parse(Console.ReadLine());
-double lev;
-lev = usd * 1.79549;
-Console.WriteLine(lev);
