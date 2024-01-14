2ef53b1a2c1103a2b858bd97bebd29dd620454fb
TotevM <mtotev53@gmail.com>
2024-01-14 10:15:02 +0200

    Added excercises

---
+++ Programming Basics with C# - June 2023/First Steps In Coding - Exercise/ConsoleApp1/Deposit Calculator/Program.cs
@@ -0,0 +1,9 @@
+﻿double suma = double.Parse(Console.ReadLine());
+int months = int.Parse(Console.ReadLine());
+double interest = double.Parse(Console.ReadLine());
+
+double lihva = suma * (interest / 100);
+double mesechnalihva = lihva / 12;
+double obshto = suma + (mesechnalihva * months);
+
+Console.WriteLine(obshto);
---
+++ Programming Basics with C# - June 2023/First Steps In Coding - Exercise/ConsoleApp1/Fish Tank/Program.cs
@@ -0,0 +1,8 @@
+﻿int shirina = int.Parse(Console.ReadLine());
+int duljina = int.Parse(Console.ReadLine());
+int visochina = int.Parse(Console.ReadLine());
+double extra=double.Parse(Console.ReadLine());
+
+double bigv = shirina * visochina * duljina;
+double V = bigv - bigv * extra / 100;
+Console.WriteLine(V/1000);
