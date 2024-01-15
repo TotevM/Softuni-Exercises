7440b336fc10c868323dfa844cbc82ed06b95438
TotevM <mtotev53@gmail.com>
2024-01-15 21:55:59 +0200

    Added exercises

---
+++ Programming Basics with C# - June 2023/3. Conditional Statements - Lab/07. Area of Figures/Program.cs
@@ -0,0 +1,28 @@
+﻿string figure = Console.ReadLine();
+
+if (figure == "square")
+{ 
+    double a = double.Parse(Console.ReadLine());
+    double S = a * a;
+    Console.WriteLine($"{S:f3}");
+}
+if (figure == "rectangle")
+{ 
+    double a = double.Parse(Console.ReadLine());
+    double b = double.Parse(Console.ReadLine());
+    double S = a * b;
+    Console.WriteLine($"{S:f3}");
+}
+if (figure == "circle")
+{ 
+    double r=double.Parse(Console.ReadLine());
+    double S=r * r*Math.PI;
+    Console.WriteLine($"{S:f3}");
+}
+if (figure == "triangle")
+{ 
+double a=double.Parse(Console.ReadLine());
+    double h=double.Parse(Console.ReadLine());
+    double S = a * h / 2;
+    Console.WriteLine($"{S:f3}");
+}
