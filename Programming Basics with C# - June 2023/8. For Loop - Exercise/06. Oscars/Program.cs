32c52bb2bcb878e9af299e3ab9bc635b67af2ba8
TotevM <mtotev53@gmail.com>
2024-01-22 00:01:37 +0200

    Added exercises

---
+++ Programming Basics with C# - June 2023/8. For Loop - Exercise/06. Oscars/Program.cs
@@ -0,0 +1,22 @@
+﻿string name = Console.ReadLine();
+double salary = double.Parse(Console.ReadLine());
+int n = int.Parse(Console.ReadLine());
+double suma = salary;
+
+for (int i = 0; i < n; i++)
+{
+    string namejuri = Console.ReadLine();
+    double points = double.Parse(Console.ReadLine());
+
+    double tochki = (namejuri.Length * points) / 2;
+    suma += tochki;
+    if (suma > 1250.5)
+    {
+        Console.WriteLine($"Congratulations, {name} got a nominee for leading role with {suma:F1}!");
+        break;
+    }
+}
+if (suma <= 1250.5)
+{
+    Console.WriteLine($"Sorry, {name} you need {(1250.5 - suma):F1} more!");
+}
---
+++ Programming Basics with C# - June 2023/8. For Loop - Exercise/07. Trekking Mania/Program.cs
@@ -0,0 +1,22 @@
+﻿int groups = int.Parse(Console.ReadLine());
+double musala = 0;
+double monblan = 0;
+double kilimanjaro = 0;
+double k2 = 0;
+double everest = 0;
+int count = 0;
+for (int i = 0; i < groups; i++)
+{
+    int people = int.Parse(Console.ReadLine());
+    if (people > 0 && people <= 5) { musala += people; }
+    if (people > 5 && people <= 12) { monblan += people; }
+    if (people > 12 && people <= 25) { kilimanjaro += people; }
+    if (people > 25 && people <= 40) { k2 += people; }
+    if (people >= 41) { everest += people; }
+    count += people;
+}
+Console.WriteLine($"{(musala / count * 100):F2}%");
+Console.WriteLine($"{(monblan / count * 100):F2}%");
+Console.WriteLine($"{(kilimanjaro / count * 100):F2}%");
+Console.WriteLine($"{(k2 / count * 100):F2}%");
+Console.WriteLine($"{(everest / count * 100):F2}%");
