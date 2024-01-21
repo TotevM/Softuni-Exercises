c661714a231e1b2ce13c363180ef8440f3cbbfef
TotevM <mtotev53@gmail.com>
2024-01-22 00:02:11 +0200

    Added exercise

---
+++ Programming Basics with C# - June 2023/8. For Loop - Exercise/08. Tennis Ranklist/Program.cs
@@ -0,0 +1,15 @@
+﻿int n = int.Parse(Console.ReadLine());
+double start = double.Parse(Console.ReadLine());
+double points = 0;
+double wins = 0;
+
+for (int i = 0; i < n; i++)
+{
+    string place = Console.ReadLine();
+    if (place == "W") { start += 2000; points += 2000; wins += 1; }
+    if (place == "F") { start += 1200; points += 1200; }
+    if (place == "SF") { start += 720; points += 720; }
+}
+Console.WriteLine($"Final points: {Math.Floor(start)}");
+Console.WriteLine($"Average points: {Math.Floor(points / n)}");
+Console.WriteLine($"{(wins / n * 100):F2}%");
