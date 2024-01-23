70f43d480ed021ba114cf312e3b4c993d2799437
TotevM <mtotev53@gmail.com>
2024-01-23 20:15:36 +0200

    Added exercises

---
+++ Programming Basics with C# - June 2023/9. While Loop - Lab/07. Min Number/Program.cs
@@ -0,0 +1,12 @@
+﻿double min = 10000000;
+string number = Console.ReadLine();
+
+while (number != "Stop")
+{
+    if (double.Parse(number) < min)
+    {
+        min = double.Parse(number);
+    }
+    number = Console.ReadLine();
+}
+Console.WriteLine(min);
---
+++ Programming Basics with C# - June 2023/9. While Loop - Lab/08. Graduation/Program.cs
@@ -0,0 +1,36 @@
+﻿                string name = Console.ReadLine();
+                int currentClass = 1;
+                int excluded = 0;
+                double sumGrades = 0;
+                bool isExcluded = false;
+
+                while (currentClass <= 12)
+                {
+                    double grade = double.Parse(Console.ReadLine());
+
+                    if (grade < 4.00)
+                    {
+                        excluded++;
+                    }
+                    else if (grade >= 4.00)
+                    {
+                        currentClass++;
+                        sumGrades += grade;
+                    }
+                    if (excluded >= 2)
+                    {
+                        isExcluded = true;
+                        break;
+                    }
+                }
+
+                double averageGrade = sumGrades / 12;
+
+                if (isExcluded==true)
+                {
+                    Console.WriteLine($"{name} has been excluded at {currentClass} grade");
+                }
+                else
+                {
+                    Console.WriteLine($"{name} graduated. Average grade: {averageGrade:f2}");
+                }
