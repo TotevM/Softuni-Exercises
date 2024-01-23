85024a47c8ead9786f0d5e0ea2bac73ae1544671
TotevM <mtotev53@gmail.com>
2024-01-23 20:15:09 +0200

    Added problems

---
+++ Programming Basics with C# - June 2023/9. While Loop - Lab/05. Account Balance/Program.cs
@@ -0,0 +1,14 @@
+﻿string vnoska = Console.ReadLine();
+double sum = 0;
+while (vnoska != "NoMoreMoney")
+{
+    if (double.Parse(vnoska) < 0)
+    {
+        Console.WriteLine("Invalid operation!");
+        break;
+    }
+    Console.WriteLine($"Increase: {double.Parse(vnoska):F2}");
+    sum += double.Parse(vnoska);
+    vnoska = Console.ReadLine();
+}
+Console.WriteLine($"Total: {sum:F2}");
---
+++ Programming Basics with C# - June 2023/9. While Loop - Lab/06. Max Number/Program.cs
@@ -0,0 +1,12 @@
+﻿double max = -10000000;
+string number = Console.ReadLine();
+
+while (number != "Stop")
+{
+    if (double.Parse(number) > max)
+    {
+        max = double.Parse(number);
+    }
+    number = Console.ReadLine();
+}
+Console.WriteLine(max);
