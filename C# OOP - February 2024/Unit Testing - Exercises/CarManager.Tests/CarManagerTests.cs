9a2ba9bfd038e88b43d86eb5f1c1b099e439198d
TotevM <mtotev53@gmail.com>
2024-04-04 21:29:36 +0300

    Edited some excercises

--- C# OOP - February 2024/Unit Testing - Exercises/CarManager.Tests/CarManagerTests.cs
+++ C# OOP - February 2024/Unit Testing - Exercises/CarManager.Tests/CarManagerTests.cs
@@ -1,10 +1,95 @@
 namespace CarManager.Tests
 {
+    using System;
     using NUnit.Framework;
 
     [TestFixture]
     public class CarManagerTests
     {
+        private const string validMake = "VW";
+        private const string validModel = "Golf";
+        private const double validFuelConsumption = 1;
+        private const double validFuelCapacity = 3;
 
+        private const string nonValidMake = "";
+        private const string nonValidModel = "";
+        private const double nonValidFuelConsumption = -1;
+        private const double nonValidFuelCapacity = -1;
+
+        private Car car;
+
+        [SetUp]
+        public void SetUp()
+        {
+            car = new Car(validMake, validModel, validFuelConsumption, validFuelCapacity);
+        }
+
+        [Test]
+        public void testCtor_ShouldWork()
+        {
+            Assert.That(car, Is.Not.Null);
+            Assert.That(car.Make == validMake);
+            Assert.That(car.Model == validModel);
+            Assert.That(car.FuelConsumption == validFuelConsumption);
+            Assert.That(car.FuelCapacity == validFuelCapacity);
+            Assert.That(car.FuelAmount == 0);
+        }
+
+        [Test]
+        public void MakeProperty_SetterWorks_ShouldThrowEx()
+        {
+            Assert.Throws<ArgumentException>(() =>
+            car = new Car(nonValidMake, validModel, validFuelConsumption, validFuelCapacity));
+        }
+
+        [Test]
+        public void ModelProperty_SetterWorks_ShouldThrowEx()
+        {
+            Assert.Throws<ArgumentException>(() =>
+            car = new Car(validMake, nonValidModel, validFuelConsumption, validFuelCapacity));
+        }
+
+        [Test]
+        public void FuelConsumptionProperty_SetterWorks_ShouldThrowEx()
+        {
+            Assert.Throws<ArgumentException>(() =>
+            car = new Car(validMake, validModel, nonValidFuelConsumption, validFuelCapacity));
+        }
+
+        [Test]
+        public void FuelAmountProperty_SetterWorks_ShouldThrowEx()
+        {
+            Assert.Throws<ArgumentException>(() =>
+            car = new Car(validMake, validModel, validFuelConsumption, nonValidFuelCapacity));
+        }
+
+        [Test]
+        public void RefuelMethod_WithNegativeNumber_ShouldThrownAnException()
+        {
+            Assert.Throws<ArgumentException>(() =>
+               car.Refuel(-1));
+        }
+
+        [Test]
+        public void RefuelMethod_RefuelingToTheTop_ShouldThrownAnException()
+        {
+            car.Refuel(validFuelCapacity + 1);
+            Assert.That(car.FuelCapacity == validFuelCapacity);
+        }
+
+        [Test]
+        public void Drive_CantReachDestination_ShouldThrowException()
+        {
+            Assert.Throws<InvalidOperationException>(() =>
+            car.Drive(400));
+        }
+
+        [Test]
+        public void Drive_WorksCorrectly()
+        {
+            car.Refuel(validFuelCapacity);
+            car.Drive(100);
+            Assert.That(car.FuelAmount+1 == car.FuelCapacity);
+        }
     }
 }
--- C# OOP - February 2024/Unit Testing - Exercises/DatabaseExtended.Tests/ExtendedDatabaseTests.cs
+++ C# OOP - February 2024/Unit Testing - Exercises/DatabaseExtended.Tests/ExtendedDatabaseTests.cs
@@ -1,14 +1,15 @@
 namespace DatabaseExtended.Tests
 {
+    using System;
     using ExtendedDatabase;
     using NUnit.Framework;
-    using System;
 
     [TestFixture]
     public class ExtendedDatabaseTests
     {
         private Database database;
-        private Person[] maxPeopleArr;
+        private const string validName = "pesho";
+        private const long validID = 123;
 
         [SetUp]
         public void SetUp()
@@ -17,31 +18,57 @@ namespace DatabaseExtended.Tests
         }
 
         [Test]
-        public void ConstructorWorks() 
+        public void Constructor_NoParameters_Works()
         {
-            for (int i = 0; i < 16; i++)
-            {
-                database.Add(new Person(i, $"prsn{i}"));
-            }
+            Assert.That(database, Is.Not.Null);
+            Assert.That(database.Count, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void Constructor_WithParameters_Works()
+        {
+            database = new Database(new Person(validID, validName));
+            Assert.That(database, Is.Not.Null);
+            Assert.That(database.Count, Is.EqualTo(1));
         }
 
         [Test]
         public void AddRange_ExceedLimit_ShouldThrow()
         {
+            Person[] exceedingAmount = new Person[17];
+
+
+            Assert.Throws<ArgumentException>(() =>
+            database = new Database(exceedingAmount));
+        }
+
+        //[Test]
+        //public void AddMethod_ExceedingLimit_ShouldThrowAnExeption()
+        //{
+        //    Person[] people = new Person[16];
+
+        //    database = new Database(people);
+        //    Person person = new Person(validID, validName);
+
+        //    Assert.Throws<InvalidOperationException>(() => database.Add(new Person(213, "dsad")));
+        //}
+
+
+        [Test]
+        public void Add_Method_WhenDatabaseIsFull_ShouldThrowInvalidOperationException()
+        {
             for (int i = 0; i < 16; i++)
             {
-                database.Add(new Person(i, $"prsn{i}"));
+                database.Add(new Person(i + 1, $"User{i + 1}"));
             }
 
-
-            Assert.Throws<InvalidOperationException>(() =>
-            database.Add(new Person(34, $"prsn{34}")));
+            Assert.Throws<InvalidOperationException>(() => database.Add(new Person(17, "User17")));
         }
 
         [Test]
         public void AddMethod_DuplicatingUsername_ShouldThrowAnExeption()
         {
-            Person p1 = new Person(2342,"nadya");
+            Person p1 = new Person(2342, "nadya");
             database.Add(p1);
 
             Person p2 = new Person(25432, "nadya");
@@ -61,9 +88,18 @@ namespace DatabaseExtended.Tests
         [Test]
         public void RemoveMethod_ShouldWork()
         {
-            database=new Database(new Person(2342, "nadya"));
+            database = new Database(new Person(validID, validName));
             database.Remove();
-            Assert.IsTrue(database.Count==0);
+            Assert.IsTrue(database.Count == 0);
+        }
+
+        [Test]
+        public void RemoveMethod_CountIsZero_ShouldThrowAnException()
+        {
+            database = new Database();
+            Assert.Throws<InvalidOperationException>(() =>
+            database.Remove()
+            );
         }
 
         [Test]
@@ -88,6 +124,28 @@ namespace DatabaseExtended.Tests
             database = new Database(new Person(2342, "nadya"));
             Assert.Throws<InvalidOperationException>(() =>
             database.FindByUsername("Nadya"));
+        }
+
+        [Test]
+        public void FindUsernameMethod_Works()
+        {
+            Person[] people = new Person[] { new Person(validID, validName), new Person(validID + 1, validName + "1") };
+            database = new Database(people);
+            Person Jack=database.FindByUsername(validName);
+
+            Assert.That(Jack.UserName == people[0].UserName);
+            Assert.That(Jack.Id == people[0].Id);
+        }
+
+        [Test]
+        public void FindIDMethod_Works()
+        {
+            Person[] people = new Person[] { new Person(validID, validName), new Person(validID + 1, validName + "1") };
+            database = new Database(people);
+            Person Jack = database.FindById(validID);
+
+            Assert.That(Jack.UserName == people[0].UserName);
+            Assert.That(Jack.Id == people[0].Id);
         }
 
         [Test]
