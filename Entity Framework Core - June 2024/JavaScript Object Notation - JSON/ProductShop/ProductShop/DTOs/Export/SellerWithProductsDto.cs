5dcd172454ef6cb9dcb50c4ab286e93876d13106
TotevM <mtotev53@gmail.com>
2024-07-31 11:21:50 +0300

    Addeed Database

---
+++ Entity Framework Core - June 2024/JavaScript Object Notation - JSON/ProductShop/ProductShop/DTOs/Export/SellerWithProductsDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProductShop.Models;
+
+namespace ProductShop.DTOs.Export
+{
+    public class SellerWithProductsDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public IEnumerable<SoldProductsDto> SoldProducts { get; set; }
+    }
+}
--- Entity Framework Core - June 2024/JavaScript Object Notation - JSON/ProductShop/ProductShop/StartUp.cs
+++ Entity Framework Core - June 2024/JavaScript Object Notation - JSON/ProductShop/ProductShop/StartUp.cs
@@ -1,9 +1,181 @@
-﻿namespace ProductShop
+﻿using System.Text.Json;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using ProductShop.Data;
+using ProductShop.DTOs.Export;
+using ProductShop.Models;
+
+namespace ProductShop
 {
     public class StartUp
     {
         public static void Main()
         {
+            ProductShopContext context = new ProductShopContext();
+            ////1
+            //string userText = File.ReadAllText("../../../Datasets/users.json");
+            //Console.WriteLine(ImportUsers(context, userText));
+
+            ////2
+            //string productsText = File.ReadAllText("../../../Datasets/products.json");
+            //Console.WriteLine(ImportProducts(context, productsText));
+
+            ////3
+            //string categoriesText = File.ReadAllText("../../../Datasets/categories.json");
+            //Console.WriteLine(ImportCategories(context, categoriesText));
+
+            //4
+            //string catProdText = File.ReadAllText("../../../Datasets/categories-products.json");
+            //Console.WriteLine(ImportCategoryProducts(context, catProdText));
+
+            Console.WriteLine(GetUsersWithProducts(context));
+
+
+        }
+
+        public static string ImportUsers(ProductShopContext context, string inputJson)
+        {
+            List<User>? users = JsonConvert.DeserializeObject<List<User>>(inputJson);
+
+            context.Users.AddRange(users);
+            context.SaveChanges();
+
+            return $"Successfully imported {users.Count}";
+        }
+        public static string ImportProducts(ProductShopContext context, string inputJson)
+        {
+            List<Product>? products = JsonConvert.DeserializeObject<List<Product>>(inputJson);
+
+            context.Products.AddRange(products);
+            context.SaveChanges();
+
+            return $"Successfully imported {products.Count}";
+        }
+        public static string ImportCategories(ProductShopContext context, string inputJson)
+        {
+            List<Category>? categories = JsonConvert.DeserializeObject<List<Category>>(inputJson);
+            categories.RemoveAll(c => c.Name == null);
+            context.Categories.AddRange(categories);
+            context.SaveChanges();
+
+            return $"Successfully imported {categories.Count}";
+        }
+        public static string ImportCategoryProducts(ProductShopContext context, string inputJson)
+        {
+            List<CategoryProduct>? catProds = JsonConvert.DeserializeObject<List<CategoryProduct>>(inputJson);
+
+            context.CategoriesProducts.AddRange(catProds);
+            context.SaveChanges();
+
+            return $"Successfully imported {catProds.Count}";
+        }
+        public static string GetProductsInRange(ProductShopContext context)
+        {
+            var products = context.Products
+                .Where(x => x.Price >= 500 && x.Price <= 1000)
+                .Select(x => new ProductDTO
+                {
+                    Name = x.Name,
+                    Price = x.Price,
+                    Seller = $"{x.Seller.FirstName} {x.Seller.LastName}"
+
+                }).OrderBy(x=>x.Price).ToList();
+
+            return SerializeObjectWithJsonSettings(products);
+        }
+        public static string GetSoldProducts(ProductShopContext context)
+        {
+            var users = context.Users
+                .Where(u => u.ProductsSold.Any(p => p.BuyerId != null))
+                .Select(x => new SellerWithProductsDto()
+                {
+                    FirstName = x.FirstName,
+                    LastName = x.LastName,
+                    SoldProducts = x.ProductsSold
+                        .Select(p => new SoldProductsDto()
+                        {
+                            Name = p.Name,
+                            Price = p.Price,
+                            BuyerFirstName = p.Buyer.FirstName,
+                            BuyerLastName = p.Buyer.LastName
+                        })
+                }).OrderBy(x=>x.LastName).ThenBy(x=>x.FirstName).ToList();
+
+            return SerializeObjectWithJsonSettings(users);
+        }
+        public static string GetCategoriesByProductsCount(ProductShopContext context)
+        {
+            var categories=context.Categories
+                .Select(x=> new
+                {
+                    Category=x.Name,
+                    ProductsCount=x.CategoriesProducts.Count,
+                    AveragePrice = x.CategoriesProducts
+                    .Average(cp => cp.Product.Price)
+                    .ToString("f2"),
+                    TotalRevenue = x.CategoriesProducts
+                        .Sum(cp => cp.Product.Price)
+                        .ToString("f2")
+                })
+                .OrderByDescending(x => x.ProductsCount)
+                .ToList();
+
+
+            return SerializeObjectWithJsonSettings(categories);
+        }
+
+        public static string GetUsersWithProducts(ProductShopContext context)
+        {
+            var users = context.Users
+                .Where(u => u.ProductsSold.Any(p => p.BuyerId != null && p.Price != null))
+                .Select(u => new
+                {
+                    u.FirstName,
+                    u.LastName,
+                    u.Age,
+                    SoldProducts = u.ProductsSold
+                        .Where(p => p.BuyerId != null && p.Price != null)
+                        .Select(p => new
+                        {
+                            p.Name,
+                            p.Price
+                        })
+                        .ToArray()
+                })
+                .OrderByDescending(u => u.SoldProducts.Length)
+                .ToArray();
+
+            var output = new
+            {
+                UsersCount = users.Length,
+                Users = users.Select(u => new
+                {
+                    u.FirstName,
+                    u.LastName,
+                    u.Age,
+                    SoldProducts = new
+                    {
+                        Count = u.SoldProducts.Length,
+                        Products = u.SoldProducts
+                    }
+                })
+
+            };
+            return SerializeObjectWithJsonSettings(output);
+        }
+
+
+
+
+        private static string SerializeObjectWithJsonSettings(object obj)
+        {
+            var settings = new JsonSerializerSettings()
+            {
+                NullValueHandling = NullValueHandling.Ignore,
+                ContractResolver = new CamelCasePropertyNamesContractResolver(),
+                Formatting = Formatting.Indented
+            };
+            return JsonConvert.SerializeObject(obj, settings);
         }
     }
 }
