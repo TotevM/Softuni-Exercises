e18e266c71aff80077f2a97bfb84b2aebb8b3ca0
Metodi Totev <mtotev53@gmail.com>
2024-09-02 21:01:27 +0300

    Permutation ex

---
+++ Algorithms Fundamentals with C# - May 2023/Combinatorial Problems/PermutationsWithoutRepetitions/Program.cs
@@ -0,0 +1,36 @@
+﻿
+namespace PermutationsWithoutRepetitions
+{
+    internal class Program
+    {
+        private static string[] arr;
+        static void Main(string[] args)
+        {
+            arr = Console.ReadLine().Split().ToArray();
+            Permute(0);
+        }
+
+        private static  void Permute(int index)
+        {
+            if (index>=arr.Length)
+            {
+                Console.WriteLine(string.Join(" ", arr));
+                return;
+            }
+
+            Permute(index+1);
+
+            for (int i = index+1; i < arr.Length; i++)
+            {
+                Swap(index, i);
+                Permute(index + 1);
+                Swap(index, i);
+            }
+        }
+
+        private static void Swap(int index, int i)
+        {
+            (arr[index], arr[i]) = (arr[i], arr[index]);
+        }
+    }
+}
