0c7b8397be5f722aefbdc011ddd68e5955f52f74
TotevM <mtotev53@gmail.com>
2024-09-10 22:46:32 +0300

    Variation Exercise

---
+++ Algorithms Fundamentals with C# - May 2023/Combinatorial Problems/VariationsWithoutRepetition/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace VariationsWithoutRepetition
+{
+    internal class Program
+    {
+        private static string[] arr;
+        static void Main(string[] args)
+        {
+            
+            string[] arr = Console.ReadLine().Split().ToArray();
+            int k = int.Parse(Console.ReadLine());
+
+            List<string> variations = new List<string>();
+            FindVariations(arr, k, "", new bool[arr.Length]);
+        }
+
+        static void FindVariations(string[] elements, int k, string current, bool[] used)
+        {
+            if (current.Length == k)
+            {
+                Console.WriteLine(current);
+                return;
+            }
+
+            for (int i = 0; i < elements.Length; i++)
+            {
+                if (!used[i])
+                {
+                    used[i] = true;
+                    FindVariations(elements, k, current + elements[i], used);
+                    used[i] = false;
+                }
+            }
+        }
+    }
+}
