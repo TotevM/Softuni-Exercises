232f20285bc5d045343dd1cfd119230acb41d3b3
Metodi Totev <mtotev53@gmail.com>
2024-10-20 16:10:59 +0300

    Finished greedy algorithms exercises

--- Algorithms Fundamentals with C# - May 2023/Searching, Sorting and Greedy Algorithms - Lab/SumOfCoins/Program.cs
+++ Algorithms Fundamentals with C# - May 2023/Searching, Sorting and Greedy Algorithms - Lab/SumOfCoins/Program.cs
@@ -1,10 +1,44 @@
-﻿namespace SumOfCoins
+﻿using System;
+using System.Collections.Generic;
+
+class Program
 {
-    internal class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        int[] coins = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        int targetSum = int.Parse(Console.ReadLine());
+
+        coins.OrderByDescending(x=>x);
+
+        Dictionary<int, int> usedCoins = new Dictionary<int, int>();
+        int totalCoins = 0;
+        int currentSum = 0;
+
+        foreach (var coin in coins)
         {
-            Console.WriteLine("Hello, World!");
+            int count = (targetSum - currentSum) / coin;
+            if (count > 0)
+            {
+                usedCoins[coin] = count;
+                totalCoins += count;
+                currentSum += count * coin;
+            }
+
+            if (currentSum == targetSum)
+                break;
+        }
+
+        if (currentSum == targetSum)
+        {
+            Console.WriteLine($"Number of coins to take: {totalCoins}");
+            foreach (var coin in usedCoins)
+            {
+                Console.WriteLine($"{coin.Value} coin(s) with value {coin.Key}");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Error");
         }
     }
 }
