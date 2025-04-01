a155eb90b333d8a5832533b4f7597bf80590e953
TotevM <mtotev53@gmail.com>
2025-04-01 22:48:24 +0300

    DOM Introduction - 2 Exercises

--- JS Front-End - February 2025/DOM Introduction - Exercise/01. Subtraction/subtraction.js
+++ JS Front-End - February 2025/DOM Introduction - Exercise/01. Subtraction/subtraction.js
@@ -1,3 +1,8 @@
 function subtract() {
-    //TODO
+    const firstNumber = document.getElementById('firstNumber');
+    const secondNumber = document.getElementById('secondNumber');
+    const resultElement = document.getElementById('result');
+
+    resultElement.textContent =
+        Number(firstNumber.value) - Number(secondNumber.value);
 }
--- JS Front-End - February 2025/DOM Introduction - Exercise/02. Pascal or Camel Case/pascal-or-camel-case.js
+++ JS Front-End - February 2025/DOM Introduction - Exercise/02. Pascal or Camel Case/pascal-or-camel-case.js
@@ -1,3 +1,20 @@
 function solve() {
-  //TODO
+    const text = document.getElementById('text');
+    const convention = document.getElementById('naming-convention');
+    const resultEl = document.getElementById('result');
+
+    debugger;
+    let words = text.value
+        .toLowerCase()
+        .split(' ')
+        .map((x) => x[0].toUpperCase() + x.slice(1))
+        .join('');
+
+    if (convention.value === 'Camel Case') {
+        resultEl.textContent = words[0].toLowerCase() + words.slice(1);
+    } else if (convention.value === 'Pascal Case') {
+        resultEl.textContent = words;
+    } else {
+        resultEl.textContent = 'Error!';
+    }
 }
