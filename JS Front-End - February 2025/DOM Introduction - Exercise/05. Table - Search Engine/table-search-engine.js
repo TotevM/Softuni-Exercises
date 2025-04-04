9415fe13bcc577877a983d93133482a717841144
Metodi Totev <mtotev53@gmail.com>
2025-04-04 22:54:14 +0300

    Update table-search-engine.js

--- JS Front-End - February 2025/DOM Introduction - Exercise/05. Table - Search Engine/table-search-engine.js
+++ JS Front-End - February 2025/DOM Introduction - Exercise/05. Table - Search Engine/table-search-engine.js
@@ -1,3 +1,22 @@
 function solve() {
-   //TODO
+   const rowElements = document.querySelectorAll('tbody tr')
+   const searchElement = document.getElementById('searchField')
+   const searchValue = searchElement.value.toLowerCase()
+
+   if (searchValue === '') {
+      return
+   }
+
+
+   for (const row of rowElements) {
+      row.classList.remove('select');
+      for (const record of row.children) {
+         if (record.textContent.toLowerCase().includes(searchValue)) {
+            row.classList.add('select')
+            break
+         }
+      }
+   }
+
+   searchElement.value = ''
 }
