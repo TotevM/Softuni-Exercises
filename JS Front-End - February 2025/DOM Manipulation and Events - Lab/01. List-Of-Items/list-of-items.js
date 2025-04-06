678a4123582f7e24e626864770706e2c46194c58
Metodi Totev <mtotev53@gmail.com>
2025-04-06 20:50:24 +0300

    Update list-of-items.js

--- JS Front-End - February 2025/DOM Manipulation and Events - Lab/01. List-Of-Items/list-of-items.js
+++ JS Front-End - February 2025/DOM Manipulation and Events - Lab/01. List-Of-Items/list-of-items.js
@@ -1,3 +1,13 @@
 function addItem() {
-    //TODO
+    const textInputElement = document.getElementById('newItemText')
+    const ulElement = document.getElementById('items')
+
+    if (textInputElement.value !== '') {
+
+        const childLiElement = document.createElement('li')
+        childLiElement.textContent = textInputElement.value
+        ulElement.appendChild(childLiElement)
+    }
+
+    textInputElement.value = ''
 }
